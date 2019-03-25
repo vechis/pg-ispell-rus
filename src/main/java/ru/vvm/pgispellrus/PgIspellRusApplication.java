a91fe4f7f0e0c053db9854ce6f20e212373b9a28
vechis <vechis@mail.ru>
2019-03-25 12:32:17 +0300

    init

---
+++ src/main/java/ru/vvm/pgispellrus/PgIspellRusApplication.java
@@ -0,0 +1,13 @@
+package ru.vvm.pgispellrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgIspellRusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgIspellRusApplication.class, args);
+    }
+
+}
